4d644eff393a359789c6fd63d48ce63ff31b1c24
Aman-Mandal <002amankumar@gmail.com>
2022-01-18 20:50:45 +0530

    fixed the key values of database

--- src/atm/simulation/Signup2.java
+++ src/atm/simulation/Signup2.java
@@ -15,7 +15,7 @@ public class Signup2 extends JFrame implements ActionListener{
     String formno;
     Signup2(String formno){
         
-        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("ASimulatorSystem/icons/logo.jpg"));
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("atm/simulation/icons/logo.jpg"));
         Image i2 = i1.getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT);
         ImageIcon i3 = new ImageIcon(i2);
         JLabel l14 = new JLabel(i3);
