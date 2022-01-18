79728b6fa05834dfcf819f17bcf81f5d144a85f3
Aman-Mandal <002amankumar@gmail.com>
2022-01-18 20:35:09 +0530

    fixed bug of logo

--- src/atm/simulation/Signup.java
+++ src/atm/simulation/Signup.java
@@ -24,7 +24,7 @@ public class Signup extends JFrame implements ActionListener{
         
         setTitle("NEW ACCOUNT APPLICATION FORM");
         
-        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("ASimulatorSystem/icons/logo.jpg"));
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("atm.simulation/icons/logo.jpg"));
         Image i2 = i1.getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT);
         ImageIcon i3 = new ImageIcon(i2);
         JLabel l11 = new JLabel(i3);
