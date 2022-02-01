f99e9619d5d834d8dfe8b0f324323e7dadad3f8e
Aman-Mandal <002amankumar@gmail.com>
2022-02-01 22:12:36 +0530

    Edited the Buttons

--- src/atm/simulation/Transactions.java
+++ src/atm/simulation/Transactions.java
@@ -28,69 +28,40 @@ public class Transactions  extends JFrame implements ActionListener{
         l1.setForeground(Color.WHITE);
         l1.setFont(new Font("System", Font.BOLD, 16));
         
-        
         b1 = new JButton("DEPOSIT");
-        b1.setFont(new Font("System", Font.BOLD,18));
-        b1.setBackground(Color.BLACK);
-        b1.setForeground(Color.WHITE);
-        
         b2 = new JButton("CASH WITHDRAWL");
-        b2.setFont(new Font("System", Font.BOLD,18));
-        b2.setBackground(Color.BLACK);
-        b2.setForeground(Color.WHITE);
-        
         b3 = new JButton("FAST CASH");
-        b3.setFont(new Font("System", Font.BOLD,18));
-        b3.setBackground(Color.BLACK);
-        b3.setForeground(Color.WHITE);
-        
         b4 = new JButton("MINI STATEMENT");
-        b4.setFont(new Font("System", Font.BOLD,18));
-        b4.setBackground(Color.BLACK);
-        b4.setForeground(Color.WHITE);
-        
         b5 = new JButton("PIN CHANGE");
-        b5.setFont(new Font("System", Font.BOLD,18));
-        b5.setBackground(Color.BLACK);
-        b5.setForeground(Color.WHITE);
-        
         b6 = new JButton("BALANCE ENQUIRY");
-        b6.setFont(new Font("System", Font.BOLD,18));
-        b6.setBackground(Color.BLACK);
-        b6.setForeground(Color.WHITE);
-        
         b7 = new JButton("EXIT");
-        b7.setFont(new Font("System", Font.BOLD,18));
-        b7.setBackground(Color.BLACK);
-        b7.setForeground(Color.WHITE);
-
         
         setLayout(null);
         
-        l1.setBounds(100,100,700,40);
-        add(l1);
+        l1.setBounds(235,400,700,35);
+        l2.add(l1);
         
-        b1.setBounds(40,250,300,60);
-        add(b1);
+        b1.setBounds(170,499,150,35);
+        l2.add(b1);
         
-        b2.setBounds(440,250,300,60);
-        add(b2);
+        b2.setBounds(390,499,150,35);
+        l2.add(b2);
         
-        b3.setBounds(40,360,300,60);
-        add(b3);
+        b3.setBounds(170,543,150,35);
+        l2.add(b3);
         
-        b4.setBounds(440,360,300,60);
-        add(b4);
+        b4.setBounds(390,543,150,35);
+        l2.add(b4);
         
-        b5.setBounds(40,470,300,60);
-        add(b5);
+        b5.setBounds(170,588,150,35);
+        l2.add(b5);
         
-        b6.setBounds(440,470,300,60);
-        add(b6);
+        b6.setBounds(390,588,150,35);
+        l2.add(b6);
         
-        b7.setBounds(240,600,300,60);
-        add(b7);
-        
+        b7.setBounds(390,633,150,35);
+        l2.add(b7);
+
         
         b1.addActionListener(this);
         b2.addActionListener(this);
@@ -101,10 +72,9 @@ public class Transactions  extends JFrame implements ActionListener{
         b7.addActionListener(this);
         
         
-        getContentPane().setBackground(Color.WHITE);
-        
-        setSize(800,800);
-        setLocation(500,90);
+        setSize(960,1080);
+        setLocation(500,0);
+        setUndecorated(true);
         setVisible(true);
        
     }
