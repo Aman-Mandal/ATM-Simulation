39186eec5459b5be586b3de348a028224ba148c4
Aman-Mandal <002amankumar@gmail.com>
2022-02-08 20:35:46 +0530

    Committed the Deposit file

--- src/atm/simulation/Deposit.java
+++ src/atm/simulation/Deposit.java
@@ -14,7 +14,7 @@ public class Deposit extends JFrame implements ActionListener{
     String pin;
     Deposit(String pin){
         this.pin = pin;
-        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("atm/simulation/icons/logo.jpg"));
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("atm/simulation/icons/atm.jpg"));
         Image i2 = i1.getImage().getScaledInstance(1000, 1180, Image.SCALE_DEFAULT);
         ImageIcon i3 = new ImageIcon(i2);
         JLabel l3 = new JLabel(i3);
